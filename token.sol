bca60bda409ab88f8ef71da8f6654de4e197a958
RaviRanjan7545 <160404290+RaviRanjan7545@users.noreply.github.com>
2024-02-18 20:09:54 +0530

    Update token.sol

--- token.sol
+++ token.sol
@@ -1,1 +1,29 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract RanjanCoin is ERC20, ERC20Burnable, Ownable {
+    constructor(address initialOwner)
+        ERC20("RanjanCoin", "RAN")
+        Ownable(initialOwner)
+    {
+        _mint(initialOwner, 1* 10 ** decimals());
+    }
+
+    function mintTokens(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burnTokens(address from, uint256 amount) public {
+        _burn(from, amount);
+    }
+
+    function transfer(address to, uint256 amount) public override returns (bool) {
+        _transfer(msg.sender, to, amount);
+        return true;
+    }
+}
